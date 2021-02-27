f842a0a06b64e472228a3eb07cd4306957b5c1f3
huangkuan <wbhuangkuan@centaline.com.cn>
2021-02-28 03:50:26 +0800

    构建

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,12 +3,20 @@ module.exports = {
   title:"akkentt",
   lang:"zh-CN",
   dest:"dist",
+  locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    '/': {
+      lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+    },
+  },
   head:[
     ['script', { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js' }],
     ['script', { src: 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.2/jquery.fancybox.min.js' }],
     ['link', { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.2/jquery.fancybox.min.css' }]
   ],
   themeConfig: {
+    lang:"zh-CN",
     smoothScroll: true,
     displayAllHeaders: false, // 默认值：false
     navbar:false,
